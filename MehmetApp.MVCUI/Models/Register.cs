b5fe4e3f28f299e7230ffa8516ddacbc0afe9f0c
uurmht <55434633+uurmht@users.noreply.github.com>
2019-10-20 21:49:06 +0300

    Resgister

--- MehmetApp.MVCUI/Models/Register.cs
+++ MehmetApp.MVCUI/Models/Register.cs
@@ -2,32 +2,41 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.Core;
 using System.Linq;
 using System.Net.Mail;
 using System.Web;
+using Newtonsoft.Json;
 
 namespace MehmetApp.MVCUI.Models
 {
     public class Register
     {
-        [Required]
+        [Required(ErrorMessage = "Adınız gereklidir.")]
         [DisplayName("Adınız")]
         public string Name { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Soyadınız gereklidir.")]
         [DisplayName("Soyadınız")]
         public string SurName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Kullanıcı Adı gereklidir.")]
         [DisplayName("Kullanıcı Adı")]
         public string UserName { get; set; }
         [Required]
+
+        
         [DisplayName("Eposta Adresiniz")]
         [EmailAddress(ErrorMessage = "Eposta adresinizi düzgün giriniz.")]
         public string Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Şifre Alanı gereklidir.")]
         [DisplayName("Şifre")]
         public string Password { get; set; }
-        [Required]
+
+        
+        [Required(ErrorMessage = "Şifre Alanı gereklidir.")]
         [DisplayName("Şifre Tekrar")]
         [Compare("Password",ErrorMessage = "Şifreler Uyuşmuyor.")]
         public string RePassword { get; set; }
